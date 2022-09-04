1d3f5eaae1442ad7ed02af758f3e74e1b073b6e5
maximolorenzo <maximolorenzoo@gmail.com>
2022-09-04 17:06:57 -0300

    desafio

---
+++ src/components/navbar/NavBar.js
@@ -0,0 +1,14 @@
+import logo from '../../assets/logo.jpg'
+import './navbar.css'
+const NavBar =() =>{
+    return (
+        <header>
+        <img src = {logo} alt = "" />
+        <a href="">Mens</a>
+        <a href="">Womens</a>
+        <a href="">Kids</a>
+        </header>
+    )
+}
+
+export default NavBar
