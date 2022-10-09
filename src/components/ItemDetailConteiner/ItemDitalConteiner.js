d6c0ed2eda3ebf0bbe14286806546090d969829f
maximolorenzo <maximolorenzoo@gmail.com>
2022-10-09 01:40:56 -0300

    desafio completado

--- src/components/ItemDetailConteiner/ItemDitalConteiner.js
+++ src/components/ItemDetailConteiner/ItemDitalConteiner.js
@@ -1,41 +1,40 @@
 import { useState, useEffect } from "react"
 import ItemDetail from "../ItemDetail/ItemDetail"
 import { useParams } from "react-router-dom"
-import { db } from "../../firebase/firebase" 
-import {doc, getDocs, collection} from "firebase/firestore"
+import { db } from "../../firebase/firebase"
+import {doc,getDoc,collection} from "firebase/firestore"
 
 const  ItemDitalConteiner = () => {
 
-    const [listProduct , setListProduct] = useState(null)
+    const [listProduct , setListProduct] = useState({})
     const [loading , setLoading] = useState (true)
 
     const {id} = useParams ()
 
-    useEffect (() =>{
-        setLoading (true)
-        const productsCollection = collection(db, "products")
-        const refDoc = doc.get(productsCollection,id)
-        console.log(id);
-        getDocs(refDoc)
-        .then((result) => {
-            return setListProduct({
-                id: result.id,
-                ...result.data()
-            })
-        })
-        .cath((e) => {
-            console.log(e);
-        })
-        .finally(() => { 
-            setLoading(false) 
-        })
-        
-    }, [id])
+    
+    useEffect(() => {
+            setLoading (true)
+            const productCollection = collection(db, "products");
+            const refDoc = doc(productCollection, id);
+            getDoc(refDoc)
+              .then((result) => {
+                setListProduct({
+                  id: result.id,
+                  ...result.data(),
+                });
+              })
+              .catch((e) => {
+                console.log(e);
+              })
+              .finally(() => {
+                setLoading(false);
+              });
+          }, [id]);
 
     
     return (
     <>
-        {loading || !listProduct ? <p>cargando...</p> :<ItemDetail listProduct = {listProduct}/>  }
+        {!loading ? <ItemDetail listProduct = {listProduct}/> : <p>cargando...</p> }
     </>
     )
 }
